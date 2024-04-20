4a53b60173e0a073169b5e650111d16f4fb45359
yumotoyoshiaki <yoshiosmile68@gmail.com>
2024-04-20 14:54:06 +0900

    fix

--- app/libs/db.ts
+++ app/libs/db.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaClient as edge } from "@prisma/client/edge";
 
 export const prisma = new PrismaClient();
+export const prismaEdge = new edge();

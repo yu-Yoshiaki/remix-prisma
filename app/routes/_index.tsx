545c3fe66a4d3934c36a0f1d51201c14aaa578a4
yumotoyoshiaki <yoshiosmile68@gmail.com>
2024-04-20 14:59:37 +0900

    test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,3 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticator } from "~/services/auth.server";
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  return await authenticator.isAuthenticated(request, {
-    failureRedirect: "/auth/login",
-  });
-};
-
 const Index = () => {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
